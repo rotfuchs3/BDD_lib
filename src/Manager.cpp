1e339f0fe3a99fff90d96b43b849e959a4c8afa1
Soeren Kwasigroch <s_kwasigro12@cs.uni-kl.de>
2018-12-10 17:15:10 +0100

    IMP: getTopVarName Test passed

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -364,7 +364,10 @@ BDD_ID Manager::nand2(const BDD_ID a,const BDD_ID b){
   Returns the label of top variable of the BDD node f
 */
 std::string Manager::getTopVarName(const BDD_ID f){
-	return "";
+	if(!isValidID(f)){
+		return "";
+	}
+	return uniqueTable.find(topVar(f))->second->label;
 }
 
 //---------------------------------------------------------------------------------------------------------
